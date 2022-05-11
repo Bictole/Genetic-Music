80267866fd9d0396d3f485620aeceea39e0bc757
Victor Simonin <victor.simonin@epita.fr>
2022-05-11 17:34:39 +0200

    [Add] new Crossover function

---
+++ src/crossover.py
@@ -0,0 +1,12 @@
+from random import randint
+
+def single_point_crossover(a, b):
+    if len(a) != len(b):
+        raise ValueError("Genomes a and b must be of same length")
+
+    length = len(a)
+    if length < 2:
+        return a, b
+
+    p = randint(1, length - 1)
+    return a[0:p] + b[p:], b[0:p] + a[p:]
