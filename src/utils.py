fa9e20ec506a8f479e81a01d5b2259fac46ec8cf
Victor Simonin <victor.simonin@epita.fr>
2022-05-12 17:40:26 +0200

    [Add] Utils and visualization function

---
+++ src/utils.py
@@ -0,0 +1,23 @@
+from fitness import population_fitness
+
+def sort_population(population, fitness_func):
+    return sorted(population, key=fitness_func, reverse=True)
+
+
+def genome_to_string(genome):
+    return "".join(map(str, genome))
+
+
+def print_stats(population, generation_id, fitness_func):
+    print("GENERATION %02d" % generation_id)
+    print("=============")
+    print("Population: [%s]" % ", ".join([genome_to_string(gene) for gene in population]))
+    print("Avg. Fitness: %f" % (population_fitness(population, fitness_func) / len(population)))
+    sorted_population = sort_population(population, fitness_func)
+    print(
+        "Best: %s (%f)" % (genome_to_string(sorted_population[0]), fitness_func(sorted_population[0])))
+    print("Worst: %s (%f)" % (genome_to_string(sorted_population[-1]),
+                              fitness_func(sorted_population[-1])))
+    print("")
+
+    return sorted_population[0]
